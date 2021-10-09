e2882da0f1961f59d80895c7beef04e293acce30
Carine Casagrande <carinecasagrande@MacBook-Air-de-Carine.local>
2021-10-09 10:35:33 -0300

    finalizado :)

--- bodyapp/lib/register/ui/screens/register_screen.dart
+++ bodyapp/lib/register/ui/screens/register_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+  RegisterScreen({Key? key}) : super(key: key);
+
   static GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +31,11 @@ class RegisterScreen extends StatelessWidget {
                   margin: EdgeInsets.only(top: 20),
                   width: 120,
                   height: 120,
-                  decoration: new BoxDecoration(
+                  decoration: BoxDecoration(
                     color: Color.fromRGBO(228, 228, 228, 1),
                     shape: BoxShape.circle,
                   ),
-                  child: new Icon(
+                  child: Icon(
                     Icons.account_circle_rounded,
                     size: 100,
                     color: Colors.teal,
@@ -46,17 +51,24 @@ class RegisterScreen extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.only(top: 20),
                           child: TextFormField(
+                            controller: name,
                             style:
                                 TextStyle(fontSize: 22.0, color: Colors.black),
-                            decoration: const InputDecoration(
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                fontSize: 16.0,
+                              ),
                               hintText: 'Nome',
                               filled: true,
                               fillColor: Color.fromRGBO(228, 228, 228, 1),
-                              border: InputBorder.none,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide.none),
                               prefixIcon: Padding(
-                                padding: EdgeInsets.all(0.0),
+                                padding: EdgeInsets.only(left: 15, right: 10),
                                 child: Icon(
                                   Icons.person_outline,
+                                  size: 40,
                                   color: Colors.teal,
                                 ),
                               ),
@@ -72,17 +84,24 @@ class RegisterScreen extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.only(top: 20),
                           child: TextFormField(
+                            controller: email,
                             style:
                                 TextStyle(fontSize: 22.0, color: Colors.black),
-                            decoration: const InputDecoration(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide.none),
+                              errorStyle: TextStyle(
+                                fontSize: 16.0,
+                              ),
                               hintText: 'Email',
                               filled: true,
                               fillColor: Color.fromRGBO(228, 228, 228, 1),
-                              border: InputBorder.none,
                               prefixIcon: Padding(
-                                padding: EdgeInsets.all(0.0),
+                                padding: EdgeInsets.only(left: 15, right: 10),
                                 child: Icon(
                                   Icons.email_outlined,
+                                  size: 40,
                                   color: Colors.teal,
                                 ),
                               ),
@@ -90,6 +109,49 @@ class RegisterScreen extends StatelessWidget {
                             validator: (String? value) {
                               if (value == null || value.isEmpty) {
                                 return 'Insira seu e-mail';
+                              }
+
+                              String p =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                              RegExp regExp = new RegExp(p);
+
+                              if (!regExp.hasMatch(value)) {
+                                return 'E-mail inválido';
+                              }
+
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: TextFormField(
+                            controller: password,
+                            style:
+                                TextStyle(fontSize: 22.0, color: Colors.black),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                              hintText: 'Senha',
+                              filled: true,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide.none),
+                              fillColor: Color.fromRGBO(228, 228, 228, 1),
+                              prefixIcon: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 10),
+                                child: Icon(
+                                  Icons.lock_outline,
+                                  size: 40,
+                                  color: Colors.teal,
+                                ),
+                              ),
+                            ),
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Insira sua senha';
                               }
                               return null;
                             },
@@ -101,46 +163,36 @@ class RegisterScreen extends StatelessWidget {
                             style:
                                 TextStyle(fontSize: 22.0, color: Colors.black),
                             obscureText: true,
-                            decoration: const InputDecoration(
-                              hintText: 'Senha',
+                            decoration: InputDecoration(
+                              errorStyle: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide.none),
+                              hintText: 'Confirmar senha',
                               filled: true,
                               fillColor: Color.fromRGBO(228, 228, 228, 1),
-                              border: InputBorder.none,
                               prefixIcon: Padding(
-                                padding: EdgeInsets.all(0.0),
+                                padding: EdgeInsets.only(left: 15, right: 10),
                                 child: Icon(
                                   Icons.lock_outline,
+                                  size: 40,
                                   color: Colors.teal,
                                 ),
                               ),
                             ),
                             validator: (String? value) {
                               if (value == null || value.isEmpty) {
-                                return 'Insira sua';
+                                return 'Insira a confirmação da senha';
                               }
+
+                              if(password.text != value){
+                                return 'As senhas não conferem';
+                              }
+
                               return null;
                             },
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 20),
-                          child: TextField(
-                            style:
-                                TextStyle(fontSize: 22.0, color: Colors.black),
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              hintText: 'Senha',
-                              filled: true,
-                              fillColor: Color.fromRGBO(228, 228, 228, 1),
-                              border: InputBorder.none,
-                              prefixIcon: Padding(
-                                padding: EdgeInsets.all(0.0),
-                                child: Icon(
-                                  Icons.lock_outline,
-                                  color: Colors.teal,
-                                ),
-                              ),
-                            ),
                           ),
                         ),
                         Column(
@@ -148,7 +200,7 @@ class RegisterScreen extends StatelessWidget {
                           children: [
                             Container(
                               margin: EdgeInsets.only(top: 20),
-                              height: 50,
+                              height: 60,
                               child: ElevatedButton(
                                 onPressed: () {
                                   if (_formKey.currentState!.validate()) {}
@@ -162,8 +214,43 @@ class RegisterScreen extends StatelessWidget {
                                   backgroundColor:
                                       MaterialStateProperty.all<Color>(
                                           Colors.teal),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        side: BorderSide.none),
+                                  ),
                                 ),
                               ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: Divider(
+                            height: 10,
+                            thickness: 5,
+                            indent: 20,
+                            endIndent: 20,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/images/facebook.png',
+                              width: 90,
+                              height: 90,
+                            ),
+                            Image.asset(
+                              'assets/images/twitter.png',
+                              width: 90,
+                              height: 90,
+                            ),
+                            Image.asset(
+                              'assets/images/google.png',
+                              width: 90,
+                              height: 90,
                             ),
                           ],
                         ),
